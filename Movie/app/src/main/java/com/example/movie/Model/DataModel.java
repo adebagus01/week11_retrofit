aaada5a8fbab7a7372c7737b94eecf39aecb0141
Ade Bagus Pratama <55870942+adebagus01@users.noreply.github.com>
2021-11-22 21:21:39 +0700

    Create DataModel.java

---
+++ Movie/app/src/main/java/com/example/movie/Model/DataModel.java
@@ -0,0 +1,38 @@
+package com.example.movie.Model;
+
+public class DataModel {
+    private int id;
+    private String nama, deskripsi, tahun;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getDeskripsi() {
+        return deskripsi;
+    }
+
+    public void setDeskripsi(String deskripsi) {
+        this.deskripsi = deskripsi;
+    }
+
+    public String getTahun() {
+        return tahun;
+    }
+
+    public void setTahun(String tahun) {
+        this.tahun = tahun;
+    }
+}
